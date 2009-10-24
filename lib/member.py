83b4040bef200843bd201fd690e2fdf48e540abc
Rickard Lindroth <rickard@eatingbits.se>
2009-10-24 16:11:25 +0200

    Removed files

--- lib/member.py
+++
@@ -1,9 +0,0 @@
-class Member(object):
-  def __init__(self, firstname, lastname, email, id = None):
-    self.id = id
-    self.firstname = firstname
-    self.lastname = lastname
-    self.email = email
-    #personnr, postnr, postort ,address, medlemsnr, email, rfid. 
-  def parameters_to_array(self):
-    return [self.firstname, self.lastname, self.email]
